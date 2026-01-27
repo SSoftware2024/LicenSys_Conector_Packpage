9cab18fe0cce3b385d735d7046f1a9f9dc3ea8bb
Soluções Software <ssolucoessoftware@gmail.com>
2026-01-27 18:16:17 -0300

    conectando a api

--- src/Services/ConectorService.php
+++ src/Services/ConectorService.php
@@ -1,10 +1,19 @@
 <?php
+
 namespace TiagoAlves\LicenSysConectorPackpage\Services;
+
+use Illuminate\Support\Facades\Http;
 
 final class ConectorService
 {
-    private string $urlApi = 'https://api.licensys.com.br';
-    private string $apiKey;
+    private string $urlApi = '';
+    private string $apiKey = '';
+
+    public function __construct()
+    {
+        $this->urlApi = env('LICENSYS_API_URL', 'http://127.0.0.1:9000/api/');
+        $this->apiKey = env('LICENSYS_API_KEY','teste');
+    }
 
     public function getUrlApi(): string
     {
@@ -13,12 +22,11 @@ final class ConectorService
 
     public function setUrlApi(string $urlApi, bool $overwrite = false): void
     {
-        if(!empty($this->urlApi) && $overwrite === true){
+        if (!empty($this->urlApi) && $overwrite === true) {
             $this->urlApi = $urlApi;
-        }else if(empty($this->urlApi)){
+        } else if (empty($this->urlApi)) {
             $this->urlApi = $urlApi;
         }
-
     }
 
     public function getApiKey(): string
@@ -31,11 +39,25 @@ final class ConectorService
         $this->apiKey = $apiKey;
     }
 
-    public function connect(): bool
+    public function checkUrl(): bool
     {
-        //enviar para conexao, com chave de api
-        //retornar token
-        // Lógica de conexão com a API LicenSys
-        return true;
+        $headers = true;
+        try {
+            get_headers($this->urlApi . 'check-api');
+        } catch (\Throwable $th) {
+            $headers = false;
+        }
+        return $headers;
+    }
+
+    public function connect()
+    {
+        if ($this->checkUrl() === true) {
+            $response = Http::get("{$this->urlApi}connect");
+            //enviar para conexao, com chave de api, uuid
+            //retornar token
+            // Lógica de conexão com a API LicenSys
+            ds($response->json());
+        }
     }
 }
