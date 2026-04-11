b2a7bbdde8fad0da295e4fbcc3db60dd2f293d2d
Soluções Software <ssolucoessoftware@gmail.com>
2026-04-11 16:18:50 -0300

    envio de tranferencia

---
+++ src/Services/TransferService.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace TiagoAlves\LicenSysConectorPackpage\Services;
+
+use TiagoAlves\LicenSysConectorPackpage\Utils\LicenSysHttpUtils;
+
+final class TransferService
+{
+    private LicenSysHttpUtils $httpLicenSys;
+    public function __construct()
+    {
+        $this->httpLicenSys = new LicenSysHttpUtils('1|kOU9ETzGcIYSIPnLdzerPUVnULw9sU2rWiqw8DB5d6ea678b');
+    }
+    public function send($company_uuid, $historic_company_id, $pix_origin_name, $file) 
+    {
+        $realPath = $file->getRealPath();
+        $new_name_ext = $file->hashName();
+        $http = $this->httpLicenSys->getHttpWithHeaders();
+        $response = $http->attach(
+            'pix_receipt_photo', file_get_contents($realPath), $new_name_ext
+        )->post('transfer/proccess',[
+            'company_uuid' => $company_uuid,
+            'historic_company_id' => $historic_company_id,
+            'pix_origin_name' => $pix_origin_name,
+        ]); 
+        return $response->successful()?$response->json():$response->body();
+    }
+}
