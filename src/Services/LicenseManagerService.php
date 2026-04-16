b0d92eaf08bca2c1db22f82c225ce1f4f15919b6
Soluções Software <ssolucoessoftware@gmail.com>
2026-04-15 21:15:37 -0300

    bearer tokens em service

--- src/Services/LicenseManagerService.php
+++ src/Services/LicenseManagerService.php
@@ -1,9 +1,18 @@
 <?php
+
 namespace TiagoAlves\LicenSysConectorPackpage\Services;
+
+use TiagoAlves\LicenSysConectorPackpage\Utils\LicenSysHttpUtils;
 
 final class LicenseManagerService
 {
-    public function getDataLicense(){}
-    public function getMonths(){}
 
+    private LicenSysHttpUtils $httpLicenSys;
+    public function __construct(string $bearerToken)
+    {
+        $this->httpLicenSys = new LicenSysHttpUtils($bearerToken);
+    }
+
+    public function getDataLicense() {}
+    public function getMonths() {}
 }
